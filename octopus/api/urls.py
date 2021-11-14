f05309071afbb47e840cc6994386b431a83f4e2b
yixin <yixin.wang.dev@gmail.com>
2021-11-14 22:46:39 +0100

    refacto: remove useless

--- octopus/api/urls.py
+++ octopus/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from . import views
 from .handlers.movie.movie_controller import MovieController
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("movie/", MovieController.as_view()),
 ]
